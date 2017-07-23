bfd696d6c39bcc5cac50ef5ca4e260fc506a9014
tsoo6464 <tsoo6464@gmail.com>
2017-07-23 21:11:30 +0800

    20170723初建專案
    APP IOS新專案

---
+++ BJWorkIOS/BJWorkIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BJWorkIOS
+//
+//  Created by Nan on 2017/7/23.
+//  Copyright © 2017年 iii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
